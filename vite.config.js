2e13e59178ca98c21ed5ae7f091750a3c3e5d735
KizunaG <hecto@LAPTOP-IUQ31NPB>
2025-10-15 20:44:52 -0600

    waos

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 👇 ESTA LÍNEA ES LA IMPORTANTE
+// Debe coincidir con el nombre EXACTO del repositorio de GitHub
 export default defineConfig({
   plugins: [react()],
+  base: '/Dise-oresponsive/',   // 👈 OJO con la barra al inicio y al final
 })
