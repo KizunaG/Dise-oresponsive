646b52a10e89c95ab6cdf0872e6af67856c6b5df
KizunaG <hecto@LAPTOP-IUQ31NPB>
2025-10-15 20:58:27 -0600

    build listo

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 👇 ESTA LÍNEA ES LA IMPORTANTE
-// Debe coincidir con el nombre EXACTO del repositorio de GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/Dise-oresponsive/',   // 👈 OJO con la barra al inicio y al final
+  base: '/Dise-oresponsive/', // 👈 EXACTO como se llama el repo
 })
